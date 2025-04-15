4de0a19dff0f90767d21b86e6c97e170da50033a
Jimi Väyrynen <jimieemeli@gmail.com>
2025-04-15 10:07:37 +0300

    Toimiva CardValidator

--- src/main/scala/CardValidator.scala
+++ src/main/scala/CardValidator.scala
@@ -1,27 +1,47 @@
+import scala.collection.immutable.*
 
+object CardValueHelper:
+
+  // Muuntaa kortin numeerseen arvon
+  def cardValue(card: PlayingCard): Int = card.value match
+    case "Ace" => 1
+    case "Jack" => 11
+    case "Queen" => 12
+    case "King" => 13
+    case n => n.toIntOption.getOrElse(0)
 
 object CardValidator:
 
-  // Voiko pelaajan kortit yhdistä pöydällä oleviin kortteihin
-  def canTakeCards(playerCard: PlayingCard, tableCards: List[PlayingCard]): Boolean =
-    val matchingCards = tableCards.filter(_.value == playerCard.value)
+  // kaikki mahdolliset kombinaatiot
+  def findValidTakes(playerCard: PlayingCard, tableCards: List[PlayingCard]): List[List[PlayingCard]] =
+    val target = CardValueHelper.cardValue(playerCard)
 
-    matchingCards.nonEmpty
+    val allCombos = tableCards.toSet.subsets().filter(_.nonEmpty).map(_.toList).toList
 
-  // voiko pelaaja laittaa kortin pöytään
-  def canPlayCard(playerCard: PlayingCard, tableCards: List[PlayingCard]): Boolean =
-    tableCards.isEmpty
+    val validCombos = allCombos.filter(c => c.map(CardValueHelper.cardValue).sum == target)
 
-  // onko laillinen siirto
+    def nonOverLapping(remaining: List[List[PlayingCard]], acc: List[PlayingCard]): List[List[PlayingCard]] =
+      remaining match
+        case Nil => List(acc)
+        case head :: tail =>
+          if head.forall(!acc.contains(_)) then
+            val withThis = nonOverLapping(tail, acc ++ head)
+            val withoutThis = nonOverLapping(tail, acc)
+            withThis ++ withoutThis
+          else
+            nonOverLapping(tail, acc)
+
+    val merged = nonOverLapping(validCombos,List()).filter(_.nonEmpty)
+    val sameValue = tableCards.filter(_.value == playerCard.value)
+    val valueMatch = if sameValue.nonEmpty then List(sameValue) else Nil
+
+    merged ++ valueMatch
+
+  // Onko siirto validi eli voiko joko ottaa tai pelata pöytään
   def validateMove(playerCard: PlayingCard, tableCards: List[PlayingCard]): Boolean =
-    (canTakeCards(playerCard, tableCards), canPlayCard(playerCard, tableCards)) match
-      case (true, _) =>
-        println(s"Player can take cards with ${playerCard.value} from the table.")
-        true
-      case (_, true) =>
-        println(s"Player can play card ${playerCard.value} on the table.")
-        true
-      case _ =>
-        println(s"Invalid move with card ${playerCard.value}. Try again.")
-        false
+    findValidTakes(playerCard, tableCards).nonEmpty || canPlayCard(playerCard, tableCards)
+
+  // Kortin pelaaminen pöytään on aina sallittua (jos ei oteta mitään)
+  def canPlayCard(playerCard: PlayingCard, tableCards: List[PlayingCard]): Boolean = true
+
 
